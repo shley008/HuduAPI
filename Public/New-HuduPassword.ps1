e2c90fff91a7b829495f3232652e6ba007c131a9
shley008 <61912791+shley008@users.noreply.github.com>
2023-01-26 13:40:30 -0600

    Add FolderID

--- Public/New-HuduPassword.ps1
+++ Public/New-HuduPassword.ps1
@@ -22,7 +22,9 @@ function New-HuduPassword {
     [String]$Username = '',
     [String]$Description = '',
     [Alias("password_type")]
-    [String]$PasswordType = ''
+    [String]$PasswordType = '',
+    [Alias("password_folder_id")]
+    [String]$PasswordFolder = ''
   )
   
   $AssetPassword = [ordered]@{asset_password = [ordered]@{} }
@@ -57,6 +59,10 @@ function New-HuduPassword {
 
   if ($PasswordType) {
     $AssetPassword.asset_password.add('password_type', $PasswordType)
+  }
+
+  if ($PasswordFolder) {
+    $assetPassword.asset_password.add('password_folder_id', $PasswordFolder)
   }
   
   $JSON = $AssetPassword | ConvertTo-Json -Depth 10
